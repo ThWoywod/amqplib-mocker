6df4153a3f9386eb1f34f7af06d4e1d6b681b050
thwoywod <tw@web-computing.de>
2018-04-17 18:01:59 +0200

    add EventEmitter to Connection

--- src/Connection.ts
+++ src/Connection.ts
@@ -1,10 +1,11 @@
-import { Channel } from './Channel';
+import { EventEmitter } from "events";
+import { Channel } from "./Channel";
 
-export interface IConnection {
+export interface IConnection extends NodeJS.EventEmitter {
   createChannel: () => Promise<Channel>;
 } 
 
-export class Connection implements IConnection {
+export class Connection extends EventEmitter implements IConnection {
   private channel: Channel = new Channel();
   async createChannel() {
     return new Promise<Channel>((resolve, reject) => {
